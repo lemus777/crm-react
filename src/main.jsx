bfcdd1bcd6e0fa5e33cb7b505a905d9ae5c82fff
lemus <lemus777@gmail.com>
2023-07-19 21:03:29 +0200

    router, loader, formulario, action

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+// como diferentes componentes pueden tener diferentes loader, para evitar conflictos renombramos al importar
+import Index, { loader as clientesLoader } from './pages/Index'
+import NuevoCliente, { action as nuevoClienteAction } from './pages/NuevoCliente'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+        loader: clientesLoader
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente />,
+        action: nuevoClienteAction
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
